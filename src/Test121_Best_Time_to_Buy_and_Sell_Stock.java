c0a8aa3fd58270077dfd825f1d38c7f1346a1198
Blooossom <taegyeong19@gmail.com>
2022-12-30 14:10:23 +0900

    feat:Added LeetCode algorithm

--- src/Test121_Best_Time_to_Buy_and_Sell_Stock.java
+++ src/Test121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -14,23 +14,11 @@ public class Test121_Best_Time_to_Buy_and_Sell_Stock {
         System.out.println(maxProfit(arr));
     }
     static int maxProfit(int[] prices){
-        int n = prices.length;
-        int[][] dp = new int[n + 1][n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            dp[0][i] = prices[i - 1];
-        }
-
-        for (int i = 1; i < n + 1; i++) {
-            dp[i][0] = prices[i - 1];
-        }
+        int min = Integer.MAX_VALUE;
         int max = 0;
-        for (int i = 2; i < dp.length; i++) {
-            for (int j = 1; j < i; j++) {
-                dp[i][j] = dp[i][0] - dp[0][j];
-                if(max < dp[i][j]) {
-                    max = dp[i][j];
-                }
-            }
+        for (int i = 0; i < prices.length; i++) {
+            min = Math.min(min, prices[i]);
+            max = Math.max(max, prices[i] - min);
         }
         return max;
     }
